179a8e209f83f1fcf7715d9cda66a00d5fa7c38e
Suman Karmakar <sumankarmakar@Sumans-MacBook-Air.local>
2023-02-09 09:22:05 +0530

    added routing for getting the data from db

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const authController = require("../controllers/authControllers");
 
 const router = Router();
 
+router.get("/availability", authController.train_availability);
 router.post("/trains", authController.train_register);
 
 module.exports = router;
